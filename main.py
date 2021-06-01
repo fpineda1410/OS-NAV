22de726ad5ea4828661de2531c2a842d44a8d8c5
fpineda1410 <fpineda147@outlook.com>
2021-06-01 12:14:23 -0600

    Modified

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import os
 import fnmatch
 
-'''Basic Ransomware Behavior
-
-A ransomware is a script or set of instructions that is designed to acces files in the host files
-and modify them for malicious purposes
+'''Basic delete file
 
 #EDUCATIONAL PURPOSES ONLY
 
